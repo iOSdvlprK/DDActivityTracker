f3047f0daddd52ad2839d265e068a2576464b58d
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-01-27 22:55:23 +0900

    Initial Commit

---
+++ DDActivityTracker/DDActivityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDActivityTrackerApp.swift
+//  DDActivityTracker
+//
+//  Created by joe on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct DDActivityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
