59b8fd3ce3c7cf85673e15b5c1f7b5ac1b3fc5da
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-01-28 23:29:25 +0900

    pie chart and donut chart using SectorMark

--- DDActivityTracker/ActivityView.swift
+++ DDActivityTracker/ActivityView.swift
@@ -15,13 +15,51 @@ struct ActivityView: View {
     
     @Environment(\.modelContext) private var context
     
+    @State private var newName: String = ""
+    @State private var hoursPerDay: Double = 0.0
+    @State private var currentActivity: Activity? = nil
+    
+    @State private var selectCount: Int?
+    
     var body: some View {
         NavigationStack {
             VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Activity Tracker")
+                Chart {
+                    let isSelected: Bool = true
+                    
+                    ForEach(activities) { activity in
+                        SectorMark(
+                            angle: .value("Activities", activity.hoursPerDay),
+                            innerRadius: .ratio(0.6),
+                            outerRadius: isSelected ? 1.05 : 0.95,
+                            angularInset: 1
+                        )
+                        .foregroundStyle(Color.red)
+                        .cornerRadius(20)
+                    }
+                    
+                    SectorMark(
+                        angle: .value("value", 5),
+                        innerRadius: .ratio(0.6),
+                        outerRadius: .ratio(0.95),
+                        angularInset: 1)
+                    .foregroundStyle(Color.red)
+                    .cornerRadius(20)
+                    
+                    SectorMark(
+                        angle: .value("value", 3),
+                        innerRadius: .ratio(0.6),
+                        outerRadius: .ratio(1.05),
+                        angularInset: 1)
+                    .foregroundStyle(Color.blue)
+                    
+                    SectorMark(
+                        angle: .value("value", 17),
+                        innerRadius: .ratio(0.6),
+                        outerRadius: .ratio(0.95),
+                        angularInset: 1)
+                    .foregroundStyle(Color.green)
+                }
             }
             .padding()
             .navigationTitle("Activity Tracker")
